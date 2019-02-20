e7456df037474d8aee10478e07c66b202e3c9915
Noah Albertsen <albertsennk@g.cofc.edu>
2019-02-19 23:35:59 -0500

    Added landscape layout, app style, screenshots of functionality

--- app/src/main/java/com/example/android/tipcalculatoralbertsen/MainActivity.java
+++ app/src/main/java/com/example/android/tipcalculatoralbertsen/MainActivity.java
@@ -4,11 +4,14 @@ import android.content.DialogInterface;
 import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.KeyEvent;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.RadioButton;
+import android.widget.TextView;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -21,6 +24,9 @@ public class MainActivity extends AppCompatActivity {
     private EditText party;
     private EditText bill;
     private EditText tip;
+    private TextView finalTip;
+    private TextView totalPrice;
+    private TextView pricePP;
     private int rbutton;
     private float amount;
     private int numPeople;
@@ -33,7 +39,19 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         reset = findViewById(R.id.resetButton);
+        reset.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                reset();
+            }
+        });
         calculate = findViewById(R.id.calculate);
+        calculate.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                calculate();
+            }
+        });
 
         party = findViewById(R.id.numPeople);
         party.setOnKeyListener(mKeyListener);
@@ -41,6 +59,10 @@ public class MainActivity extends AppCompatActivity {
         bill.setOnKeyListener(mKeyListener);
         tip = findViewById(R.id.customTip);
         tip.setOnKeyListener(mKeyListener);
+
+        finalTip = findViewById(R.id.finalTip);
+        totalPrice = findViewById(R.id.billTotal);
+        pricePP = findViewById(R.id.pricePP);
 
         ten = findViewById(R.id.radioTen);
         fifteen = findViewById(R.id.radioFifteen);
@@ -148,14 +170,40 @@ public class MainActivity extends AppCompatActivity {
         super.onSaveInstanceState(savedInstanceState);
         savedInstanceState.putInt("button", rbutton);
 
-        amount = Float.parseFloat(bill.getText().toString());
-        savedInstanceState.putFloat("billAmount", amount);
+        String billString = bill.getText().toString();
+        Log.d("rotate", billString);
+        if (!billString.equals("")) {
+            amount = Float.parseFloat(billString);
+            savedInstanceState.putFloat("billAmount", amount);
+        }
+        else {
+            savedInstanceState.putFloat("billAmount", 0);
+        }
 
-        numPeople = Integer.parseInt(party.getText().toString());
-        savedInstanceState.putInt("numPeople", numPeople);
+//        amount = Float.parseFloat(bill.getText().toString());
+//        savedInstanceState.putFloat("billAmount", amount);
 
-        tipAmount = Integer.parseInt(tip.getText().toString());
-        savedInstanceState.putInt("tipAmount", tipAmount);
+        String numPeopleText = party.getText().toString();
+        if (!numPeopleText.equals("")) {
+            numPeople = Integer.parseInt(numPeopleText);
+            savedInstanceState.putInt("numPeople", numPeople);
+        }
+        else {
+            savedInstanceState.putInt("numPeople", 0);
+        }
+//        numPeople = Integer.parseInt(party.getText().toString());
+//        savedInstanceState.putInt("numPeople", numPeople);
+
+        String tipText = tip.getText().toString();
+        if (!tipText.equals("")) {
+            tipAmount = Integer.parseInt(tipText);
+            savedInstanceState.putInt("tipAmount", tipAmount);
+        }
+        else {
+            savedInstanceState.putInt("tipAmount", 0);
+        }
+//        tipAmount = Integer.parseInt(tip.getText().toString());
+//        savedInstanceState.putInt("tipAmount", tipAmount);
     }
 
     @Override
@@ -165,11 +213,13 @@ public class MainActivity extends AppCompatActivity {
         rbutton = savedInstanceState.getInt("button");
 
         amount = savedInstanceState.getFloat("billAmount");
-
+        bill.setText(String.valueOf(amount));
 
         numPeople = savedInstanceState.getInt("numPeople");
+        party.setText(String.valueOf(numPeople));
 
         tipAmount = savedInstanceState.getInt("tipAmount");
+        tip.setText(String.valueOf(tipAmount));
     }
 
     public void onRadioButtonClicked(View view) {
@@ -181,8 +231,8 @@ public class MainActivity extends AppCompatActivity {
                     rbutton = 1;
                     if (!bill.getText().toString().equals("") &&
                             !party.getText().toString().equals("")) {
-                        if ((Float.parseFloat(bill.getText().toString()) > 1
-                                && Integer.parseInt(party.getText().toString()) > 1)){
+                        if ((Float.parseFloat(bill.getText().toString()) >= 1
+                                && Integer.parseInt(party.getText().toString()) > 0)){
                             calculate.setEnabled(true);
                         }
                     }
@@ -197,7 +247,7 @@ public class MainActivity extends AppCompatActivity {
                     if (!bill.getText().toString().equals("") &&
                             !party.getText().toString().equals("")) {
                         if ((Float.parseFloat(bill.getText().toString()) > 1
-                                && Integer.parseInt(party.getText().toString()) > 1)){
+                                && Integer.parseInt(party.getText().toString()) > 0)){
                             calculate.setEnabled(true);
                         }
                     }
@@ -212,7 +262,7 @@ public class MainActivity extends AppCompatActivity {
                     if (!bill.getText().toString().equals("") &&
                             !party.getText().toString().equals("")) {
                         if ((Float.parseFloat(bill.getText().toString()) > 1
-                                && Integer.parseInt(party.getText().toString()) > 1)){
+                                && Integer.parseInt(party.getText().toString()) > 0)){
                             calculate.setEnabled(true);
                         }
                     }
@@ -228,7 +278,7 @@ public class MainActivity extends AppCompatActivity {
                             !party.getText().toString().equals("") &&
                             !tip.getText().toString().equals("")) {
                         if ((Float.parseFloat(bill.getText().toString()) > 1
-                                && Integer.parseInt(party.getText().toString()) > 1
+                                && Integer.parseInt(party.getText().toString()) > 0
                                 && Integer.parseInt(tip.getText().toString()) > 1)){
                             calculate.setEnabled(true);
                         }
@@ -242,6 +292,52 @@ public class MainActivity extends AppCompatActivity {
     }
 
     public void calculate() {
+        double billTotal;
+        double tempBill;
+        double tipPercent = 0;
+        double tipAmount;
+        double ppTotal;
+        switch (rbutton) {
+            case 1:
+//                Log.d("calculate", "case 1");
 
+                tipPercent = 0.1;
+                break;
+            case 2:
+//                Log.d("calculate", "case 2");
+
+                tipPercent = 0.15;
+                break;
+            case 3:
+//                Log.d("calculate", "case 3");
+
+                tipPercent = 0.2;
+                break;
+            case 4:
+//                Log.d("calculate", "case 4");
+                tipPercent = Integer.parseInt(tip.getText().toString()) / 100.0;
+//                Toast toast = Toast.makeText(getApplicationContext(), "custom tip" + String.valueOf(tipPercent), Toast.LENGTH_SHORT);
+//                toast.show();
+                break;
+        }
+        tempBill = Float.parseFloat(bill.getText().toString());
+        tipAmount = tempBill * tipPercent;
+        billTotal = tempBill + tipAmount + 0.0;
+        ppTotal = billTotal / (Integer.parseInt(party.getText().toString()) + 0.0);
+
+//        Toast toast = Toast.makeText(getApplicationContext(), String.valueOf(tipAmount), Toast.LENGTH_SHORT);
+//        toast.show();
+        finalTip.setText(String.format("Tip: $%.2f", tipAmount));
+        totalPrice.setText(String.format("Bill Total: $%.2f", billTotal));
+        pricePP.setText(String.format("Price per Person: $%.2f", ppTotal));
+    }
+
+    public void reset() {
+        bill.setText(null);
+        party.setText(null);
+        tip.setText(null);
+        finalTip.setText(null);
+        totalPrice.setText(null);
+        pricePP.setText(null);
     }
 }
